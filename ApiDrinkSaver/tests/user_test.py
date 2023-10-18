5d3d5ba87618c9dcff343db6a4f8022a5a6241d4
steeve0304 <zychsteeve4@gmail.com>
2023-10-18 14:22:22 +0200

    update test

--- ApiDrinkSaver/tests/user_test.py
+++ ApiDrinkSaver/tests/user_test.py
@@ -105,7 +105,7 @@ class PasswordResetConfirmTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class UserDetailsTest(APITestCase):
+class UserDetailTest(APITestCase):
     @classmethod
     def as_view(cls):
         view = UserProfileView.as_view()
