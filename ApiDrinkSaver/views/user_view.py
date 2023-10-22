a76ae968bf873fb7ce0340ac1ebc5e6acb49055c
steeve0304 <zychsteeve4@gmail.com>
2023-10-22 13:59:37 +0200

    add view user

--- ApiDrinkSaver/views/user_view.py
+++ ApiDrinkSaver/views/user_view.py
@@ -1,22 +1,22 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from allauth.account.views import SignupView, LoginView
+from rest_framework import viewsets, permissions
+from ApiDrinkSaver.models.user import CustomUser, UserProfile, BarOwnerProfile
+from ApiDrinkSaver.serializers.user_serializer import (CustomUserSerializer, UserProfileSerializer,
+                                                       BarOwnerProfileSerializer)
 
-# Exemples de vues pour l'inscription, la connexion, etc.
-@api_view(["POST"])
-def custom_user_signup(request):
-# Vue personnalisée pour l'inscription
-    # Utilisez SignupView de Django-Allauth ou personnalisez la vôtre.
 
-@api_view(["POST"])
-def custom_user_login(request):
-# Vue personnalisée pour la connexion
-    # Utilisez LoginView de Django-Allauth ou personnalisez la vôtre.
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def user_profile(request):
-# Vue pour afficher le profil de l'utilisateur actuellement connecté
-    # Vous pouvez personnaliser cela pour afficher des informations spécifiques au type d'utilisateur.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BarOwnerProfileViewSet(viewsets.ModelViewSet):
+    queryset = BarOwnerProfile.objects.all()
+    serializer_class = BarOwnerProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
