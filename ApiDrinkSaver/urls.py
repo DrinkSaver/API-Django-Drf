49e695db883e71c4ec5a2db79bcec508d7e5b6cc
steeve0304 <zychsteeve4@gmail.com>
2023-11-19 19:50:46 +0100

    add view favorites

--- ApiDrinkSaver/urls.py
+++ ApiDrinkSaver/urls.py
@@ -3,7 +3,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from ApiDrinkSaver.views.userLambda_view import UserLambdaListView, UserLambdaDetailView
 from ApiDrinkSaver.views.bar_view import BarListCreateView, BarRetrieveUpdateDeleteView
 from ApiDrinkSaver.views.drinkPrice_view import DrinkPriceListCreateView, DrinkPriceRetrieveUpdateDeleteView
-from ApiDrinkSaver.views.priceModification_view import PriceModificationListCreateView, PriceModificationRetrieveUpdateDeleteView
+from ApiDrinkSaver.views.priceModification_view import PriceModificationListCreateView, \
+    PriceModificationRetrieveUpdateDeleteView
+from ApiDrinkSaver.views.favorites_view import FavoriteItemListCreateView, FavoriteItemRetrieveUpdateDeleteView
+
 
 urlpatterns = [
     # Authentification
@@ -18,4 +21,14 @@ urlpatterns = [
     path('drink-prices/', DrinkPriceListCreateView.as_view(), name='drink-price-list-create'),
     path('drink-prices/<int:pk>/', DrinkPriceRetrieveUpdateDeleteView.as_view(), name='drink-price-retrieve-update'
                                                                                       '-delete'),
+    # Price Modifications
+    path('price-modifications/', PriceModificationListCreateView.as_view(), name='price-modification-list-create'),
+    path('price-modifications/<int:pk>/', PriceModificationRetrieveUpdateDeleteView.as_view(), name='price'
+                                                                                                    '-modification'
+                                                                                                    '-retrieve-update'
+                                                                                                    '-delete'),
+    # Favorites
+    path('favorites/', FavoriteItemListCreateView.as_view(), name='favorite-item-list-create'),
+    path('favorites/<int:pk>/', FavoriteItemRetrieveUpdateDeleteView.as_view(),
+         name='favorite-item-retrieve-update-delete'),
 ]
--- ApiDrinkSaver/views/favorites_view.py
+++ ApiDrinkSaver/views/favorites_view.py
@@ -1,21 +1,38 @@
-from rest_framework.exceptions import ValidationError
 from rest_framework import generics
-from ApiDrinkSaver.modules.pagination import CustomPagination
-from ApiDrinkSaver.serializers.favorites_serializer import FavoriteItemSerializer
+from rest_framework.permissions import IsAuthenticated
 from ApiDrinkSaver.models.favorites import FavoriteItem
+from ApiDrinkSaver.serializers.favorites_serializer import FavoriteItemSerializer
+from rest_framework.exceptions import PermissionDenied
 
 
-class FavoriteItemList(generics.ListAPIView):
+class FavoriteItemListCreateView(generics.ListCreateAPIView):
     queryset = FavoriteItem.objects.all()
     serializer_class = FavoriteItemSerializer
-    pagination_class = CustomPagination
-
-
-class FavoriteItemCreateView(generics.CreateAPIView):
-    serializer_class = FavoriteItemSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        item = serializer.validated_data.get('item')
-        if not item:
-            raise ValidationError("L'élément favori n'existe pas.")
-        serializer.save()
+        # Vérifiez si l'utilisateur est un utilisateur lambda
+        if self.request.user.is_lambda:
+            serializer.save(user=self.request.user)
+        else:
+            raise PermissionDenied("Vous n'avez pas la permission de créer un favori.")
+
+
+class FavoriteItemRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FavoriteItem.objects.all()
+    serializer_class = FavoriteItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        # Vérifiez si l'utilisateur est un utilisateur lambda
+        if self.request.user.is_lambda:
+            serializer.save()
+        else:
+            raise PermissionDenied("Vous n'avez pas la permission de mettre à jour ce favori.")
+
+    def perform_destroy(self, instance):
+        # Vérifiez si l'utilisateur est un utilisateur lambda
+        if self.request.user.is_lambda:
+            instance.delete()
+        else:
+            raise PermissionDenied("Vous n'avez pas la permission de supprimer ce favori.")
